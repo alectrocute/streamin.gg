4ed21b583f876b33a9571cd547bbe1e8fa4c2380
Alec Armbruster <alecsamuelarmbruster@gmail.com>
2018-06-29 16:23:23 -0700

    Update convert.sh

--- convert.sh
+++ convert.sh
@@ -10,6 +10,6 @@ cd /var/www/streamingg/dl/ # change me
 find -mmin +59 -type f -exec rm -fv {} \
 # do mp3 conversion with youtube-dl
 youtube-dl --extract-audio --audio-format mp3 --prefer-ffmpeg --output %\(id\)s.%\(ext\)s ${URL}
-youtube-dl --output %\(id\)s.%\(ext\)s ${URL}
+youtube-dl -f 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/mp4' --output %\(id\)s.%\(ext\)s ${URL}
 # return last string to php!
 youtube-dl -x --get-id ${URL}
